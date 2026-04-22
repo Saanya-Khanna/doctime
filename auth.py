901517ac1feee5dd0c0958e3efa831c5260328ff
Saanya Khanna <saanyakhanna16@gmail.com>
2026-04-22 15:40:19 -0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -15,29 +15,28 @@ USERS = {
 
 def login():
 
-    st.markdown("## 🏥 DocTime")
+    st.markdown("""
+    <h1 style='text-align:center;'>🏥 DocTime</h1>
+    <p style='text-align:center;color:gray;'>Book doctors instantly</p>
+    """, unsafe_allow_html=True)
 
-    with st.container():
-        st.markdown("### Sign in")
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
 
-        email = st.text_input("Email")
-        password = st.text_input("Password", type="password")
+    if st.button("Login"):
 
-        if st.button("Login"):
+        user = USERS.get(email)
 
-            user = USERS.get(email)
+        if user and user["password"] == password:
 
-            if user and user["password"] == password:
+            st.session_state.logged_in = True
+            st.session_state.role = user["role"]
+            st.session_state.user = user
 
-                st.session_state.logged_in = True
-                st.session_state.role = user["role"]
-                st.session_state.user = user
+            st.rerun()
 
-                st.rerun()
-
-            else:
-                st.error("Invalid credentials")
-
+        else:
+            st.error("Invalid login")
 
 def logout():
     st.session_state.logged_in = False
